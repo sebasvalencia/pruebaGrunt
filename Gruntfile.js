fedf855171a9151367f401ed8c6feb74e6fe7954
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-10-17 17:38:15 -0500

    Se crea el archivo gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt){
+    //Aca va la definición y configuración de tareas
+    /*
+        Se definen en tres partes:
+        Configuración de tareas
+        Cargar plugins
+        Definición de tareas personalizadas
+    */
+
+    
+
+};
